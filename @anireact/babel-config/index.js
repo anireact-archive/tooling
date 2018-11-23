22112ae5e6120d1b397c10ca2530c2ef4e24c483
Marina Miyaoka <miyaokamarina@gmail.com>
2018-11-23 20:29:12 +0300

    fix(babel-config): Moved to custom preset.

--- @anireact/babel-config/index.js
+++ @anireact/babel-config/index.js
@@ -1,16 +1,4 @@
 module.exports = {
     ignore: [/@types/u],
-    presets: [
-        [
-            '@babel/preset-env',
-            {
-                loose: true,
-                modules: false,
-                shippedProposals: true,
-                useBuiltIns: 'usage',
-            },
-        ],
-        '@babel/preset-typescript',
-    ],
-    plugins: ['@babel/plugin-transform-runtime'],
+    presets: ['@anireact/babel-preset'],
 };
