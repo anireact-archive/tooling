b0a2e93d5e798f3a90bacd1179e3a29ea318b03b
miyaokamarina <miyaokamarina@gmail.com>
2019-02-03 18:58:12 +0300

    fix(eslint-config): Removed pointless `import/*` rules.

--- @anireact/eslint-config/index.js
+++ @anireact/eslint-config/index.js
@@ -278,34 +278,9 @@ module.exports = {
         // endregion Babel
 
         // region Import
-        'import/no-unresolved': error,
-        'import/named': error,
-        'import/default': error,
-        'import/namespace': error,
         'import/no-absolute-path': error,
-        'import/no-dynamic-require': error,
-        'import/no-webpack-loader-syntax': error,
-        'import/no-self-import': error,
-        'import/no-cycle': error,
         'import/no-useless-path-segments': error,
-
-        'import/no-named-as-default': error,
-        'import/no-named-as-default-member': error,
-        'import/no-deprecated': error,
-        'import/no-extraneous-dependencies': error,
-        'import/no-mutable-exports': error,
-
-        'import/no-commonjs': error,
-        'import/no-amd': error,
-
-        'import/first': error,
-        'import/no-duplicates': error,
-        'import/extensions': error,
-        'import/order': [error, { 'newlines-between': 'always' }],
-        'import/no-unassigned-import': error,
-        'import/no-named-default': error,
         'import/no-default-export': error,
-        'import/no-anonymous-default-export': error,
         // endregion Import
 
         // region Unicorn
